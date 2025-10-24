99665c13ff1b791ab9d08bb2c5a85a1bb1d018ae
AYUBKHAN46 <ayub7730@yahoo.com>
2025-10-24 21:42:35 +0530

    First Commit

---
+++ ex_03_Literals_variables/Lab020_Built_In_Functions.py
@@ -0,0 +1,14 @@
+# Built In - Functions
+# Pre defind given by the Python guys to you to use it.
+from math import factorial
+
+# max()
+# min()
+# print()
+print(pow(2,3))
+b = abs(-10) # Return the absolute value of the argument.
+print(b)
+c=factorial(5)
+print(c)
+d=factorial(0)
+print(d)
